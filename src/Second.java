1fb8545695d528b972cb19ec9e83d2e172dc0852
TatianaShurpyak <tshurpyak@yandex.ru>
2015-04-22 00:00:51 +0400

    First HW task - Simple app which can get 2 args and print max value

---
+++ src/Second.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// Simple app which can get 1 args as a number from [1-7] and prints Day(Use ‘switch’ here)
+
+public class Second {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+
+
+    }
+}
